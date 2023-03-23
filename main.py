f7e73cd1b64bee4fbac0948a4f6d567ae9ba4463
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2023-03-23 15:10:39 -0500

    Add initial github actions for linting, testing, and docker build testing
    Signed-off-by: Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,14 +1,74 @@
+from typing import Optional
+import argparse
 import logging
 import asyncio
 
 import ttsvc
 
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        prog='tiktoken-grpc',
+        description='tiktoken-grpc is a gRPC service that wraps the tiktoken Python library offered by OpenAI.',
+    )
+
+    parser.add_argument(
+        '-p', '--port', type=int, default=50051,
+        help='The port number that the service should listen on.',
+    )
+    parser.add_argument(
+        '--no-tls', action='store_true',
+        help='When passed in, the server will be started in insecure mode without TLS configured.',
+    )
+    parser.add_argument(
+        '--tls-cert', type=argparse.FileType('rb'),
+        help='Path to a local file containing the TLS certificate to use. Required unless --no-tls is passed in.',
+    )
+    parser.add_argument(
+        '--tls-key', type=argparse.FileType('rb'),
+        help='Path to a local file containing the TLS private key to use. Required unless --no-tls is passed in.',
+    )
+    parser.add_argument(
+        '--tls-cacert', type=argparse.FileType('rb'),
+        help='Path to a local file containing the TLS root CA certificate to use. Required if --tls-require-client-auth is passed in.',
+    )
+    parser.add_argument(
+        '--tls-require-client-auth', action='store_true',
+        help='Whether to require client authentication for connections. When passed in, --tls-cacert must also be set.',
+    )
+    parser.add_argument(
+        '--shutdown-timeout', type=int, default=15,
+        help='The number of seconds to wait during graceful shutdown.',
+    )
+
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
+    args = parse_args()
+
+    tls_opts: Optional[ttsvc.TLSOpts] = None
+    if not args.no_tls:
+        cacert: Optional[bytes] = None
+        if args.tls_cacert:
+            cacert = args.tls_cacert.read()
+        tls_opts = ttsvc.TLSOpts(
+            cert=args.tls_cert.read(),
+            private_key=args.tls_key.read(),
+            cacert=cacert,
+            require_client_auth=args.tls_require_client_auth,
+        )
+    opts = ttsvc.ServerOpts(
+        port=args.port,
+        tls_opts=tls_opts,
+        shutdown_timeout_seconds=args.shutdown_timeout,
+    )
+
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     try:
-        loop.run_until_complete(ttsvc.serve())
+        loop.run_until_complete(ttsvc.serve(opts))
     finally:
-        loop.run_until_complete(*ttsvc.cleanup_coroutines)
+        ttsvc.cleanup(loop)
         loop.close()
--- ttsvc/service_impl.py
+++ ttsvc/service_impl.py
@@ -1,4 +1,5 @@
-from typing import List, Coroutine
+from typing import List, Coroutine, Optional
+from dataclasses import dataclass
 import logging
 
 import tiktoken
@@ -7,7 +8,7 @@ from .pb2.tiktoken.v1 import tiktoken_pb2
 from .pb2.tiktoken.v1 import tiktoken_pb2_grpc
 
 # Coroutines to be invoked when the event loop is shutting down.
-cleanup_coroutines: List[Coroutine] = []
+__cleanup_coroutines: List[Coroutine] = []
 
 
 class TiktokenService(tiktoken_pb2_grpc.TiktokenServiceServicer):
@@ -16,30 +17,83 @@ class TiktokenService(tiktoken_pb2_grpc.TiktokenServiceServicer):
         request: tiktoken_pb2.NumTokensRequest,
         context: grpc.aio.ServicerContext,
     ) -> tiktoken_pb2.NumTokensResponse:
+        """
+        Use the tiktoken library to return the number of tokens in the given text based on the requested encoding.
+
+        The encoding can be determined by either the model name (if by_model_name is set on the request) or directly by
+        the encoding name (if by_name is set on the requst).
+        """
         field = request.WhichOneof("encoding")
         fv = getattr(request, request.WhichOneof("encoding"))
         if field == "by_model_name":
             encoding = tiktoken.encoding_for_model(fv)
-        else:
+        elif field == "by_name":
             encoding = tiktoken.get_encoding(fv)
+        else:
+            raise Exception(f"Unknown encoding field {field}")
         num_tokens = len(encoding.encode(request.text))
         return tiktoken_pb2.NumTokensResponse(count=num_tokens)
 
 
-async def serve() -> None:
+@dataclass
+class TLSOpts:
+    # Contents of the TLS certificate file.
+    cert: bytes
+    # Contents of the TLS private key file.
+    private_key: bytes
+    # Contents of the TLS CA cert file.
+    cacert: Optional[bytes]
+    # Whether client authentication is required. When true, cacert must not be None.
+    require_client_auth: bool
+
+
+@dataclass
+class ServerOpts:
+    # The port that the server listens on.
+    port: int
+    # The number of seconds that the server should gracefully wait during a shutdown event.
+    shutdown_timeout_seconds: int
+    # Options for setting up TLS authentication. Set to None if you do not wish to start the server with TLS auth.
+    tls_opts: Optional[TLSOpts]
+
+
+async def serve(opts: ServerOpts) -> None:
+    """
+    Main entrypoint for the tiktoken grpc service. This will start the grpc server, listening on the port specified in
+    the server opts.
+    """
+    listen_addr = f"[::]:{opts.port}"
+
     server = grpc.aio.server()
     tiktoken_pb2_grpc.add_TiktokenServiceServicer_to_server(TiktokenService(), server)
-    listen_addr = "[::]:50051"
-    server.add_insecure_port(listen_addr)
+
+    if opts.tls_opts is None:
+        server.add_insecure_port(listen_addr)
+    else:
+        server_credentials = grpc.ssl_server_credentials(
+            [(opts.tls_opts.private_key, opts.tls_opts.cert)],
+            opts.tls_opts.cacert,
+            opts.tls_opts.require_client_auth,
+        )
+        server.add_secure_port(listen_addr, server_credentials)
+
     logging.info("Starting server on %s", listen_addr)
     await server.start()
 
     async def server_graceful_shutdown():
+        """
+        Shuts down the server with configured seconds of grace period. During the grace period, the server won't accept
+        new connections and allow existing RPCs to continue within the grace period.
+        """
         logging.info("Starting graceful shutdown...")
-        # Shuts down the server with 5 seconds of grace period. During the
-        # grace period, the server won't accept new connections and allow
-        # existing RPCs to continue within the grace period.
-        await server.stop(5)
+        await server.stop(opts.shutdown_timeout_seconds)
 
-    cleanup_coroutines.append(server_graceful_shutdown())
+    __cleanup_coroutines.append(server_graceful_shutdown())
     await server.wait_for_termination()
+
+
+def cleanup(loop) -> None:
+    """
+    Clean up the server event loop by waiting for the server to gracefully shutdown.
+    """
+    loop.run_until_complete(*__cleanup_coroutines)
